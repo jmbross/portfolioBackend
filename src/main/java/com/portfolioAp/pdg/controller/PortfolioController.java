adb982c32441f0c930692647a24d93769838cadf
pitux <padanguina@gmail.com>
2022-05-14 15:49:34 -0300

    Add files via upload
    CrossOrigin

--- src/main/java/com/portfolioAp/pdg/controller/PortfolioController.java
+++ src/main/java/com/portfolioAp/pdg/controller/PortfolioController.java
@@ -5,16 +5,22 @@ import com.portfolioAp.pdg.services.IPortfolioService;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/portfolio")
+//@CrossOrigin("http://localhost:4200")
+@CrossOrigin
 
 public class PortfolioController {
     
@@ -23,33 +29,35 @@ public class PortfolioController {
     private IPortfolioService portfolioServ;
     
     List<PortfolioController> portfolio = new ArrayList();
+
+    @GetMapping ("/all")
+    @ResponseBody
+    public List<Portfolio> listarPortfolios(){
+        return portfolioServ.listarPortfolios();
+    }
     
-    @PostMapping ("new/portfolio")
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping ("/new")
     public void agregarPortfolio(@RequestBody Portfolio portfo){
         portfolioServ.newPortfolio(portfo);
     }
-   
-    @GetMapping ("/view/portfolio/{id}")
-    @ResponseBody
-    public Portfolio verPortfolio(@PathVariable Long id){
-     return portfolioServ.buscarPortfolio(id);
-    }
-    
-    @PutMapping ("/edit/portfolio/{id}")
+     
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping ("/update/{id}")
     public void editPortfolio(@RequestBody Portfolio portfo){
         portfolioServ.newPortfolio(portfo);
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping ("/delete/{id}")
     public void borrarPortfolio(@PathVariable Long id){
         portfolioServ.deletePortfolio(id);
     }
     
-    /*
-    @GetMapping ("/viewall")
+    @GetMapping ("/view/{id}")
     @ResponseBody
-    public List<Portfolio> listarPortfolios(){
-        return portfolioServ.listarPortfolios();
+    public Portfolio verPortfolio(@PathVariable Long id){
+     return portfolioServ.buscarPortfolio(id);
     }
-    */
+    
 }
